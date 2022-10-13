318f8e4282b2830477051d9a0379978f33fb3a66
caiyong <yong.cai@kapeixi.com>
2022-10-13 11:37:02 +0800

    ci

--- template/src/application/index.ts
+++
@@ -1,5 +0,0 @@
-{{#if_eq orm 'sequelize' }}
-import('./sequelize')
-{{else}}
-import('./typeorm')
-{{/if_eq}}
--- template/src/infrastucture/server/index.ts
+++ template/src/infrastucture/server/index.ts
@@ -2,15 +2,19 @@ export enum ServerType {
   {{#if_eq server 'express'}}express = 'express'{{else}}koa = 'koa'{{/if_eq}}
 }
 
+export enum OrmType {
+  {{#if_eq orm 'sequelize'}}sequelize = 'sequelize'{{else}}typeorm = 'typeorm'{{/if_eq}}
+}
+
 /**
  * @description 创建服务
  * @param port 端口
  * @param type 服务的框架
  * @returns 
  */
-export const createServer = (port: number, type: ServerType) => {
-  return import(`./${type}`).then(module => {
+export const createServer = (port: number, serverType: ServerType, ormType: OrmType) => {
+  return import(`./${serverType}`).then(module => {
     module.default.listen(port)
-    import('@/application')
+    import(`@/application/${ormType}`)
   })
 };
