3168fc613c5d69cc5cb7ec564076ec37743a0d41
caiyong <yong.cai@kapeixi.com>
2023-03-06 21:54:32 +0800

    ci: 使用rich-flow启动项目

--- utils/index.js
+++ utils/index.js
@@ -75,7 +75,7 @@ To get started:
   ${yellow(
     `${data.inPlace ? '' : `cd ${data.destDirName}\n  `}${installMsg(
       data
-    )}${lintMsg(data)}npm run dev`
+    )}${lintMsg(data)}rich run dev`
   )}`
   console.log(message)
 }
